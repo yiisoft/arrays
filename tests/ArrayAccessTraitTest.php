4451c1f6259bda353bf91a18481e2c7f920a4c46
Sergey Predvoditelev <sergey.predvoditelev@gmail.com>
2020-09-03 23:27:56 +0300

    Fix ArrayAccessTrait and add tests for him.

---
+++ tests/ArrayAccessTraitTest.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Arrays\Tests;
+
+use ArrayIterator;
+use PHPUnit\Framework\TestCase;
+
+class ArrayAccessTraitTest extends TestCase
+{
+
+    public function testIterator()
+    {
+        $object = new ArrayAccessObject();
+        $iterator = $object->getIterator();
+        $this->assertInstanceOf(ArrayIterator::class, $iterator);
+        $this->assertSame($object->data, $iterator->getArrayCopy());
+    }
+
+    public function testCount()
+    {
+        $object = new ArrayAccessObject();
+        $this->assertSame(3, $object->count());
+    }
+
+    public function testOffsetExists()
+    {
+        $object = new ArrayAccessObject();
+        $this->assertTrue($object->offsetExists('a'));
+        $this->assertFalse($object->offsetExists('x'));
+    }
+
+    public function testOffsetGet()
+    {
+        $object = new ArrayAccessObject();
+        $this->assertSame(1, $object->offsetGet('a'));
+        $this->assertNull($object->offsetGet('x'));
+    }
+
+    public function testOffsetSet()
+    {
+        $object = new ArrayAccessObject();
+        $object->offsetSet('a', 4);
+        $object->offsetSet('x', 5);
+        $this->assertSame([
+            'a' => 4,
+            'b' => 2,
+            'c' => 3,
+            'x' => 5,
+        ], $object->data);
+    }
+
+    public function testOffsetUnset()
+    {
+        $object = new ArrayAccessObject();
+        $object->offsetUnset('b');
+        $this->assertSame([
+            'a' => 1,
+            'c' => 3,
+        ], $object->data);
+    }
+}
