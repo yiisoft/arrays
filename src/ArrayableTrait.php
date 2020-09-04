12441693ee11ce81422ddab9a934ca1071cfb264
Sergey Predvoditelev <sergey.predvoditelev@gmail.com>
2020-09-04 20:43:51 +0300

    Change scope ArrayableTrait::arrayToArray to private.

--- src/ArrayableTrait.php
+++ src/ArrayableTrait.php
@@ -126,7 +126,7 @@ trait ArrayableTrait
         return $recursive ? ArrayHelper::toArray($data) : $data;
     }
 
-    protected function arrayToArray(array $array, array $fields = [], array $expand = []): array
+    private function arrayToArray(array $array, array $fields = [], array $expand = []): array
     {
         $data = [];
         $rootFields = $this->extractRootFields($fields);
