14ebe0dac1a9ef09f9f5991859746239d0efc1f5
Sergey Predvoditelev <sergey.predvoditelev@gmail.com>
2020-11-07 11:10:53 +0300

    Add phpdoc to DataModifierInterface.

--- src/Collection/Modifier/ModifierInterface/DataModifierInterface.php
+++ src/Collection/Modifier/ModifierInterface/DataModifierInterface.php
@@ -4,7 +4,14 @@ declare(strict_types=1);
 
 namespace Yiisoft\Arrays\Collection\Modifier\ModifierInterface;
 
+/**
+ * Modifiers implementing this interface are applied at transformation collection to array.
+ */
 interface DataModifierInterface extends ModifierInterface
 {
+    /**
+     * @param array $data Collection data.
+     * @return array Resulting array.
+     */
     public function apply(array $data): array;
 }
