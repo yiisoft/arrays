b73ab4a013d44bc4fcf8f84353a6bea0227f299d
arutyunyan <arutyunyan@bolid.ru>
2021-02-18 11:55:20 +0300

    added group test

---
+++ tests/ArrayHelper/GroupTest.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Arrays\Tests\ArrayHelper;
+
+use InvalidArgumentException;
+use PHPUnit\Framework\TestCase;
+use Yiisoft\Arrays\ArrayHelper;
+
+final class GroupTest extends TestCase
+{
+    public function testGroup(): void
+    {
+        $array = [
+            ['id' => '123', 'data' => 'abc'],
+            ['id' => '345', 'data' => 'def'],
+            ['id' => '345', 'data' => 'ghi'],
+        ];
+        $result = ArrayHelper::group($array, 'id');
+        $this->assertEquals(
+            [
+                '123' => [
+                  ['id' => '123', 'data' => 'abc']
+                ],
+                '345' => [
+                  ['id' => '345', 'data' => 'def'],
+                  ['id' => '345', 'data' => 'ghi'],
+                ],
+            ],
+            $result
+        );
+    }
+}
