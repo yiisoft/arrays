1244b8d42a8b381e3db4de0435293182cbad65ae
Alexey Rogachev <arogachev90@gmail.com>
2022-08-17 17:18:29 +0600

    Reorder and regroup test data sets, part 2

--- tests/ArrayHelper/ParsePathTest.php
+++ tests/ArrayHelper/ParsePathTest.php
@@ -17,11 +17,13 @@ final class ParsePathTest extends TestCase
             ['key1..key2..key3', '.', '\\', false, ['key1', '', 'key2', '', 'key3']],
             ['key1...key2...key3', '.', '\\', false, ['key1', '', '', 'key2', '', '', 'key3']],
             ['key1\.key2.key3', '.', '\\', false, ['key1.key2', 'key3']],
-            ['key1\:key2:key3', ':', '\\', false, ['key1:key2', 'key3']],
-            ['key1\.key2.key3', '.', '\\', true, ['key1\.key2', 'key3']],
             ['\.key1.key2', '.', '\\', false, ['.key1', 'key2']],
             ['key1.key2\.', '.', '\\', false, ['key1', 'key2.']],
             ['key1\..\.key2\..\.key3', '.', '\\', false, ['key1.', '.key2.', '.key3']],
+
+            ['key1\:key2:key3', ':', '\\', false, ['key1:key2', 'key3']],
+
+            ['key1\.key2.key3', '.', '\\', true, ['key1\.key2', 'key3']],
 
             ['key1\\key2\\key3', '\\', '/', false, ['key1', 'key2', 'key3']],
             ['key1\\\\key2\\\\key3', '\\', '/', false, ['key1', '', 'key2', '', 'key3']],
