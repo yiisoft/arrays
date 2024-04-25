37e8e06bed4bfbfcb597cabb3e3c76321bb22d0e
Sergei Predvoditelev <sergei@predvoditelev.ru>
2024-04-25 21:25:55 +0300

    improve ArrayAccess

---
+++ src/PsalmTraitHelper.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Arrays;
+
+/**
+ * @internal Need for correct psalm analysis.
+ * @see https://github.com/vimeo/psalm/issues/10920
+ */
+final class PsalmTraitHelper
+{
+    /** @use ArrayAccessTrait<array-key, mixed> */
+    use ArrayAccessTrait;
+
+    public array $data = [];
+}
