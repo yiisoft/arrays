14670399abd87b19285474a09a705477d2a50a1d
Sergei Predvoditelev <sergei@predvoditelev.ru>
2024-12-13 14:07:58 +0300

    Update rector.php

--- rector.php
+++ rector.php
@@ -3,11 +3,8 @@
 declare(strict_types=1);
 
 use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
-use Rector\CodingStyle\Rector\FuncCall\ArraySpreadInsteadOfArrayMergeRector;
 use Rector\Config\RectorConfig;
-use Rector\Php73\Rector\FuncCall\JsonThrowOnErrorRector;
 use Rector\Php74\Rector\Closure\ClosureToArrowFunctionRector;
-use Rector\Php81\Rector\Property\ReadOnlyPropertyRector;
 use Rector\Set\ValueObject\LevelSetList;
 
 return static function (RectorConfig $rectorConfig): void {
@@ -26,8 +23,5 @@ return static function (RectorConfig $rectorConfig): void {
 
     $rectorConfig->skip([
         ClosureToArrowFunctionRector::class,
-        ArraySpreadInsteadOfArrayMergeRector::class,
-        JsonThrowOnErrorRector::class,
-        ReadOnlyPropertyRector::class,
     ]);
 };
